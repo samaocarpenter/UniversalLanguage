1f39e24ede38a2c30a71ffbe16d58e3c5c672f55
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 12:48:08 -0500

    updated function headers

--- utils/database.py
+++ utils/database.py
@@ -1,11 +1,14 @@
 from objects import Song, Language
 
 
-def search(vocab: list[str], language: Language, n_songs: int = 3) -> list[Song]:
+def search(
+        vocab: list[str], language: Language, n_songs: int = 3, file: str = 'database.db'
+) -> list[Song]:
     """
     :param vocab: list of vocabulary words to be searched for
     :param language: language to be used
     :param n_songs: number of songs to return
+    :param file: file name for database
     :return: list of n_songs songs, each represented as a tuple containing:
         (song name, artist name)
     """
@@ -13,12 +16,13 @@ def search(vocab: list[str], language: Language, n_songs: int = 3) -> list[Song]
     raise NotImplementedError()
 
 
-def add_files(language: Language, n_files: int = 100) -> None:
+def add_files(language: Language, n_files: int = 100, file: str = 'database.db') -> None:
     """
     Adds a set amount of songs to a database.
 
     :param language: the language for the songs
     :param n_files: number of songs to be added
+    :param file: file name for database
     :return:
     """
 
