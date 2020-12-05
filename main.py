a925bdb56eef568def5a459656e2e01720154f01
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 10:19:33 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is the main script to run the entire program
+# This should be implemented LAST
+# Ideally this will allow input of either a vocab list or Duolingo information
+# Tentatively runs in terminal. In a perfect world, runs on web
+
+
+def main():
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    main()
