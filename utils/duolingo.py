0584998a65a8c1e2df02e70f88da674859d80c71
AdityaU3672 <75536724+AdityaU3672@users.noreply.github.com>
2020-12-06 02:45:12 +0530

    Update duolingo.py

--- utils/duolingo.py
+++ utils/duolingo.py
@@ -13,3 +13,14 @@ def get_vocab(language: Language, username: str, password: str, n_words: int = 1
     :return: list of vocab words as strings
     """
     raise NotImplementedError()
+
+#Non-UI implementation
+user_id = input("enter user id")
+password = input("enter password")
+lang = input("enter language")
+
+lingo = duolingo.Duolingo(user_id, password)
+abbrev = lingo.get_abbreviation_of(lang)
+myset = lingo.get_known_words(abbrev)
+
+mylist = list(myset)
