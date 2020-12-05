1a0d65f5aa46d911deab0aa7c490d81a5955e11b
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 15:18:20 -0500

    started work on music import

--- utils/music.py
+++ utils/music.py
@@ -1,5 +1,7 @@
 from typing import Generator
-from objects import Song, Language
+from utils.objects import Song, Language
+import requests
+import json
 
 
 def pull_music(language: Language, n_files: int = 100) -> Generator[Song]:
@@ -11,4 +13,21 @@ def pull_music(language: Language, n_files: int = 100) -> Generator[Song]:
     :param n_files: number of songs to bre returned
     :return: generates song objects
     """
-    raise NotImplementedError()
+    apikey = "85bc15a0e3c80f2e1112755526e4b80f"
+    url = "https://api.musixmatch.com/ws/1.1/chart.tracks.get"
+    page = 1
+    parameters = {
+        "apikey": apikey,
+        "f_lyrics_language": language.code,
+        "page_size": str(100),
+        "country": "XW",
+        "page": str(page),
+        "chart_name": 'hot'
+    }
+    while n_files > 0:
+        response = requests.get(url, params=parameters)
+        if response.status_code != 200:
+            raise ConnectionError(
+                "Musixmatch returned status code " + str(response.status_code)
+            )
+
