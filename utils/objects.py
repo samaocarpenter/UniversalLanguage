ea338476d049a3e69d46e81027930d73cf2b4af4
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 11:07:49 -0500

    added object code

--- utils/objects.py
+++ utils/objects.py
@@ -1,6 +1,17 @@
+import re
+regex = re.compile('[^a-zA-Z ]')
+
+
 class Song:
-    pass
+    def __init__(self, name: str, artist: str, lyrics: str) -> None:
+        self.name = name
+        self.artist = artist
+
+        lyrics = lyrics.lower()
+        lyrics = regex.sub('', lyrics)
+        self.lyrics = set(lyrics.split(' '))
 
 
 class Language:
-    pass
+    def __init__(self, name: str) -> None:
+        self.name = name
