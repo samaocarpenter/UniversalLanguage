e71fa448fbff1d6ee1d5496f1ab7ce55a06841ef
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 14:34:53 -0500

    added database code

--- utils/database.py
+++ utils/database.py
@@ -1,29 +1,62 @@
 from objects import Song, Language
+from music import pull_music
+import pickle
+from collections import Counter
 
 
 def search(
-        vocab: list[str], language: Language, n_songs: int = 3, file: str = 'database.db'
-) -> list[Song]:
+        vocab: list[str], language: Language, n_songs: int = 3,
+) -> list[str]:
     """
     :param vocab: list of vocabulary words to be searched for
     :param language: language to be used
     :param n_songs: number of songs to return
-    :param file: file name for database
-    :return: list of n_songs songs, each represented as a tuple containing:
-        (song name, artist name)
+    :return: list of n_songs songs, each represented as a string containing:
+        (song name) by (artist name)
     """
 
-    raise NotImplementedError()
+    try:
+        with open(language.file, 'rb') as f:
+            words = pickle.load(f)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(
+            'Language input does not have an existing associated database.\n' + repr(e)
+        )
+
+    counts = Counter()
+    for word in vocab:
+        for song in words[word]:
+            counts[song] += 1
+
+    ret = []
+    for data, _ in counts.most_common(n_songs):
+        ret.append(data)
+
+    return ret
 
 
-def add_files(language: Language, n_files: int = 100, file: str = 'database.db') -> None:
+def add_files(language: Language, n_files: int = 100) -> None:
     """
     Adds a set amount of songs to a database.
 
     :param language: the language for the songs
     :param n_files: number of songs to be added
-    :param file: file name for database
     :return:
     """
+    try:
+        with open(language.file, 'rb') as f:
+            vocabulary = pickle.load(f)
+    except FileNotFoundError:
+        vocabulary = {}
 
-    raise NotImplementedError()
+    for song in pull_music(language, n_files=n_files):
+        lyrics = song.lyrics.lower().split()
+        name = str(song)
+        for word in lyrics:
+            word = ''.join([i for i in word if i.isalpha()])
+            if word not in vocabulary:
+                vocabulary[word] = set()
+            vocabulary[word].add(name)
+
+    with open(language.file, 'wb') as f:
+        pickle.dump(vocabulary, f)
--- utils/objects.py
+++ utils/objects.py
@@ -1,14 +1,17 @@
-import re
-regex = re.compile('[^a-zA-Z ]')
-
-
 class Song:
     def __init__(self, name: str, artist: str, lyrics: str) -> None:
         self.name = name
         self.artist = artist
         self.lyrics = lyrics
 
+    def __str__(self):
+        return self.name + ' by ' + self.artist
+
 
 class Language:
     def __init__(self, name: str) -> None:
         self.name = name
+        self.file = 'databases/' + name + '.db'
+
+    def __str__(self):
+        return self.name
