1f26ea3d1cac291accf8e067077145f5942bdb48
jblovett <jblovett@live.unc.edu>
2020-12-06 11:30:05 -0500

    connecting duo.py to guiLogin.py

--- utils/guiLogin.py
+++ utils/guiLogin.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import duo
+import duolingo
 from tkinter import Button, Label, Entry
 
 master = Tk()
@@ -14,15 +16,16 @@ search = Button(master, text="Search!", padx=25, pady=25, command=None) # comman
 
 #Trying out a function to handle username and password
 
-def duo_login(username: str, password: str):
+def duo_login(username: str, password: str, language: str):
     """Takes in user login info once submit button is pressed.
     Reveals next button if login was successful. Reveals a "try again" label
     if login was unsuccessful."""
     error_label = tk.Label(text="Login failed")
     next_button = Button(master, text="next") #TODO: command SongRecs to open.
     try:
-        lingo = duolingo.Duolingo(username, password)
+        duo.get_vocab(language, username, password, -1)
         error_label.pack_forget()
+        next_button.grid(row=4, column=0)
 
     except DuolingoException("Login failed"):
             error_label.grid(row=4, column=0)
@@ -39,11 +42,17 @@ pass_entry = tk.Entry()
 pass_label.grid(row=2, column=0)
 pass_entry.grid(row=3, column=0)
 
+lang_label = tk.Label(text="Language")
+lang_entry = tk.Entry()
+lang_label.grid(row=4, column=0)
+lang_entry.grid(row=5, column=0)
+
 username = name_entry.get
 password = pass_entry.get
+language = lang_entry.get
 
 # button to submit login credentials. How to handle invalid login?
-login_button = Button(master, text="Login", command=lambda: duo_login(username, password))
+login_button = Button(master, text="Login", command=lambda: duo_login(username, password, language))
 
 
 
