47741c6bd8ad60e869a9d2debe5d89b176aaf3ab
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-05 20:08:04 -0500

    rename duolingo file

--- utils/guiSongRecs.py
+++ utils/guiSongRecs.py
@@ -1,5 +1,5 @@
 # TODO: Access login info from guiLogin.py
-# Access vocab list from duolingo.py
+# Access vocab list from duo.py
 # Display list of songs with the vocab words
 # Allow button press to select a song, then go to new window with lyrics
 # Have a logout button
