6ea1127091c22cfe5cb89a7dd39637c7883eb897
Sam Carpenter <sam.carpenter@duke.edu>
2020-12-06 12:52:50 -0500

    fixed duo

--- utils/duo.py
+++ utils/duo.py
@@ -3,7 +3,7 @@ import duolingo
 from random import shuffle
 
 
-def get_vocab(language: Language, username: str, password: str, n_words: int = 10) -> List[str]:
+def get_vocab(language: Language, username: str, password: str, n_words: int = 10) -> list[str]:
     """
     Retrieves a random number of the user's current vocab words.
     Function header may be changed depending on what authentication info Duolingo needs.
@@ -18,7 +18,7 @@ def get_vocab(language: Language, username: str, password: str, n_words: int = 1
     lingo = duolingo.Duolingo(username, password)
     my_set = lingo.get_known_words(language.code)
     my_list = list(my_set)
-    if (n_words == -1):
+    if n_words == -1:
         return my_list
     shuffle(my_list)
     return my_list[:n_words]
