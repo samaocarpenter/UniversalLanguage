5d722f5a9fce2fc4f0a44101843ab0ba8f8641d7
jblovett <jblovett@live.unc.edu>
2020-12-05 20:02:25 -0500

    Worked on guiLogin

--- utils/guiLogin.py
+++ utils/guiLogin.py
@@ -12,21 +12,40 @@ search = Button(master, text="Search!", padx=25, pady=25, command=None) # comman
 # need some way to read a string of comma-separated words
 # can use split method for space-seperated words
 
+#Trying out a function to handle username and password
+
+def duo_login(username: str, password: str):
+    """Takes in user login info once submit button is pressed.
+    Reveals next button if login was successful. Reveals a "try again" label
+    if login was unsuccessful."""
+    error_label = tk.Label(text="Login failed")
+    next_button = Button(master, text="next") #TODO: command SongRecs to open.
+    try:
+        lingo = duolingo.Duolingo(username, password)
+        error_label.pack_forget()
+
+    except DuolingoException("Login failed"):
+            error_label.grid(row=4, column=0)
+
+
 # label prompting user to enter Duolingo username and password
 name_label = tk.Label(text="Username")
 name_entry = tk.Entry()
-name_lable.pack
-name_entry.pack
+name_label.grid(row=0, column=0)
+name_entry.grid(row=1, column=0)
+
 pass_label = tk.Label(text="Password")
 pass_entry = tk.Entry()
-pass_label.pack
-pass_entry.pack
+pass_label.grid(row=2, column=0)
+pass_entry.grid(row=3, column=0)
 
+username = name_entry.get
+password = pass_entry.get
 
 # button to submit login credentials. How to handle invalid login?
-submit = Button(master, text="Login")
+login_button = Button(master, text="Login", command=lambda: duo_login(username, password))
 
 
 
 print(entry)
-#mainloop()
+mainloop()
