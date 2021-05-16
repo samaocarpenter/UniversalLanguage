ad5a7b4d0b0a6bd3389264d5cfee1c1619d4e141
Aditya Uchil <75536724+AdityaU3672@users.noreply.github.com>
2021-05-16 21:52:24 +0530

    Update guiSongRecs.py

--- utils/guiSongRecs.py
+++ utils/guiSongRecs.py
@@ -1,6 +1,6 @@
 # TODO: Access login info from guiLogin.py
 # Access vocab list from duo.py
-# Display list of songs with the vocab words
+# Display list of songs and known vocab
 # Allow button press to select a song, then go to new window with lyrics
 # Have a logout button
 
